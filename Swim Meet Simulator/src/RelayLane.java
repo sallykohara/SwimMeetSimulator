c28091185892b8768043bb279d5f5f61b7fabdd6
sally.kohara <sally.kohara@MU-C1-11.mullen.pvt.k12.co.us>
2024-10-16 12:20:46 -0600

    initial commit

---
+++ Swim Meet Simulator/src/RelayLane.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+public class RelayLane
+			{
+				static ArrayList<Times>team = new ArrayList<Times>();
+				public static void fillTeam()
+				{
+					team.add(new Times("Canada", 3 , 15, 66)); 
+					team.add(new Times("Sweden", 3, 15, 54));
+					team.add(new Times("Australia", 3, 14, 65));
+					team.add(new Times("United States", 3, 12, 23));
+					team.add(new Times("France", 3, 12, 36));
+					team.add(new Times("Italy", 3, 13, 45));
+					team.add(new Times("South Africa", 3, 14, 65));
+					team.add(new Times("Great Britain", 3, 15, 23));
+					
+				}
+				}
